f53ec18d745c051d379e2465579c33c89c58c4e1
Mananjay Shrestha <Mananjaysth111@gmail.com>
2022-02-04 17:53:04 +0545

    dot net mvc project first init

---
+++ vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            //Attribute Routing
+
+            routes.MapMvcAttributeRoutes();
+            //custom route
+            routes.MapRoute("RandomRouting",
+                "Movie/Randomaction/{year}/{month}",
+                new { Controller = "Movie", action = "Randomaction" },
+                new { year = @"2015|2016", month = @"\d{2}" }
+
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ vidly/Controllers/MovieController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+
+namespace vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+
+        //routing attribute
+        [Route("Movie/Index/{year}/{month:regex(\\d{2})}")]
+        public ActionResult Index(int year, string month)
+        {
+            return Content("Hello world" + year + month);
+        }
+
+        //default routing
+        public ActionResult Randomaction(int year, string month)
+        {
+            return Content("Hello world" + year + month);
+        }
+
+        public ActionResult Random()
+        {
+            var Movie = new Movie
+            {
+                Id=1,
+                Name = "Mananjay"
+            };
+            return View(Movie);        
+        }
+
+
+    }
+}
