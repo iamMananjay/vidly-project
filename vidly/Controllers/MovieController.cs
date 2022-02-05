d418925acd8a8a0b573e747aeb88ef7c34f1a7d1
Mananjay Shrestha <Mananjaysth111@gmail.com>
2022-02-05 14:18:24 +0545

    Action parameter view model razor view

--- vidly/Controllers/MovieController.cs
+++ vidly/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using vidly.Models;
+using vidly.ViewModel;
 
 
 namespace vidly.Controllers
@@ -25,15 +26,52 @@ namespace vidly.Controllers
             return Content("Hello world" + year + month);
         }
 
+        //passing data to the view in three way 
         public ActionResult Random()
         {
             var Movie = new Movie
             {
-                Id=1,
+                Id = 1,
                 Name = "Mananjay"
             };
-            return View(Movie);        
+
+
+            //first approach
+            return View(Movie);
+
+            //second approach view bag
+            //ViewBag.UserMovie = Movie;
+            //return View(); 
+
+            // third approach view data dictionary
+            //ViewData["Movie"] = Movie;
+            //return View();
         }
+
+        public ActionResult MovieList()
+        {
+            //Make a object of Movie Model
+            var Movie = new Movie { Name = "Lord of ring" };
+            //Make a object of customer model in form of list
+
+            var Customer = new List<Customer>
+            {
+                new Customer { CustomerName="Alina Heka"},
+                new Customer {CustomerName = "Mananjay shrestha"}
+            };
+
+
+            //Make a object of Moviecustomerviewmodel and pass both model inside the property of viewmodel
+
+            var MovieCustomerViewModel = new MovieCustomerViewModel
+            {
+                Movie = Movie,
+            Customer = Customer
+            };
+            //pass a view model
+            return View(MovieCustomerViewModel);
+        }
+
 
 
     }
---
+++ vidly/ViewModel/MovieCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+
+namespace vidly.ViewModel
+{
+    public class MovieCustomerViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public List<Customer> Customer { get; set; }
+    }
+}
